6fc0f2c866f24e3116149d2feb31edc3b6dbbcf5
Marisa <marisavandellos@gmail.com>
2024-04-24 14:35:27 -0700

    updated blue page

--- src/components/Blue.jsx
+++ src/components/Blue.jsx
@@ -1,11 +1,33 @@
-//useState controls inline style for h1
-    //style: color and font
-//buttons set state
+import { useState } from "react"
 
 export default function Blue() {
+    const [color, setColor] = useState("white");
+    const [font, setFont] = useState("Arial");
+
+    const handleColor = (event) => {
+        setColor(event.target.value)
+    }
+    const handleFont = (event) => {
+        setFont(event.target.value)
+    }
+
     return (
         <div className="blue">
-            <h1>Blue</h1>
+            <div>
+                <section>
+                    <h3>Font</h3>
+                    <button value="Courier" onClick={handleFont}>Courier</button>
+                    <button value="Comic Sans MS" onClick={handleFont}>Comic Sans MS</button>
+                    <button value="Arial" onClick={handleFont}>Arial</button>
+                </section>
+                <section>
+                    <h3>Color</h3>
+                    <button value="forestgreen" onClick={handleColor}>Green</button>
+                    <button value="black" onClick={handleColor}>Black</button>
+                    <button value="white" onClick={handleColor}>White</button>
+                </section>
+                <h1 style={{ color: color, fontFamily: font }} >Blue</h1>
+            </div>
         </div>
     )
 }
